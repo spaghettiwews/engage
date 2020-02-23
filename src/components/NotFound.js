9b14dffd2d39703797813edbd73d37bcef65e17c
spaghettiwews <spaghettiwews@outlook.com>
2020-02-23 19:49:28 +0200

    add 404 page

--- src/components/NotFound.js
+++ src/components/NotFound.js
@@ -1,11 +1,10 @@
-import React from 'react';
-import Header from './Header';
+import React from "react";
+import Header from "./Header";
 
 const NotFound = () => (
-    <React.Fragment>
-    <Header />
+  <React.Fragment>
     <h1>NotFound</h1>
-    </React.Fragment>
-)
+  </React.Fragment>
+);
 
 export default NotFound;
