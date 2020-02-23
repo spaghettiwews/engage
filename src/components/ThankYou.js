438390357f9c2ece58e70efc59f6e2f57d11df21
spaghettiwews <spaghettiwews@outlook.com>
2020-02-23 19:49:14 +0200

    add thank you page

---
+++ src/components/ThankYou.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const ThankYou = () => {
+  return (
+    <>
+      <h1>Thank you for your booking request.</h1>
+      <p>One of our technicians will be in touch with you shortly.</p>
+    </>
+  );
+};
+
+export default ThankYou;
