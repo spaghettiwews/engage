65410b22089b5bd091defa3a6641031f83f8856d
spaghettiwews <spaghettiwews@outlook.com>
2025-04-11 13:46:47 +0200

    Update BookingForm.js

--- src/components/BookingForm.js
+++ src/components/BookingForm.js
@@ -1,38 +1,7 @@
 import React, { useState } from "react";
-import "./BookingForm.css";
-import { locations } from "../siteConfig";
-import { applianceTypes } from "../siteConfig";
-import { applianceMakes } from "../siteConfig";
-import { useFormik } from "formik";
-import "react-dates/initialize";
-import "react-dates/lib/css/_datepicker.css";
-import { OPEN_UP } from "react-dates/constants";
-import { SingleDatePicker } from "react-dates";
-import TimePicker from "./TimePicker";
 import { withRouter } from "react-router-dom";
 
-// const submitForm = function(event, props) {
-//   event.preventDefault();
-//   props.history.push("/thank-you");
-// };
-
 const BookingForm = function(props) {
-  const [date, setDate] = useState(null);
-  const [focused, setFocused] = useState(false);
-  const formik = useFormik({
-    initialValues: {
-      fullName: "",
-      phoneNumber: "",
-      emailAddress: "",
-      applianceType: "",
-      applianceMake: "",
-      applianceProblem: "",
-      preferredDateForRepair: "",
-      preferredTimeForRepair: "",
-      location: ""
-    }
-  });
-
   return (
     <React.Fragment>
     <iframe
