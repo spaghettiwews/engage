d5eacb10879dba9451a5820170a608cb63019866
spaghettiwews <spaghettiwews@outlook.com>
2025-04-11 13:41:09 +0200

    Update BookingForm.js

--- src/components/BookingForm.js
+++ src/components/BookingForm.js
@@ -11,10 +11,10 @@ import { SingleDatePicker } from "react-dates";
 import TimePicker from "./TimePicker";
 import { withRouter } from "react-router-dom";
 
-const submitForm = function(event, props) {
-  event.preventDefault();
-  props.history.push("/thank-you");
-};
+// const submitForm = function(event, props) {
+//   event.preventDefault();
+//   props.history.push("/thank-you");
+// };
 
 const BookingForm = function(props) {
   const [date, setDate] = useState(null);
@@ -35,136 +35,20 @@ const BookingForm = function(props) {
 
   return (
     <React.Fragment>
-      <h2 className="form__heading">Instant Repair Booking for your </h2>
-      <form
-        name="bookings"
-        method="POST"
-        onSubmit={event => submitForm(event, props)}
-      >
-        <input type="hidden" name="form-name" value="bookings" />
-        <div className="multi-column">
-          <label>
-            Name
-            <span>What's your name?</span>
-            <input
-              id="fullName"
-              name="fullName"
-              type="text"
-              onChange={formik.handleChange}
-              value={formik.values.fullName}
-            />
-          </label>
-          <label>
-            Phone number
-            <span>What number can we contact you on?</span>
-            <input
-              id="phoneNumber"
-              name="phoneNumber"
-              type="tel"
-              onChange={formik.handleChange}
-              value={formik.values.phoneNumber}
-            />
-          </label>
-        </div>
-        <div className="multi-column">
-          <label>
-            Email address
-            <span>And your email address?</span>
-            <input
-              id="emailAddress"
-              name="emailAddress"
-              type="email"
-              onChange={formik.handleChange}
-              value={formik.values.emailAddress}
-            />
-          </label>
-          <label>
-            Location
-            <span>Where is the faulty appliance?</span>
-            <select
-              id="location"
-              name="location"
-              onChange={formik.handleChange}
-              value={formik.values.location}
-            >
-              {locations.map(location => (
-                <option key={location.label} value={location.value}>
-                  {location.label}
-                </option>
-              ))}
-            </select>
-          </label>
-        </div>
-        <div className="multi-column">
-        <label>
-            Appliance type
-            <span>What appliance are you having issues with?</span>
-            <select
-              id="applianceType"
-              name="applianceType"
-              onChange={formik.handleChange}
-              value={formik.values.applianceType}
-            >
-              {applianceTypes.map(type => (
-                <option key={type.label} value={type.value}>
-                  {type.label}
-                </option>
-              ))}
-            </select>
-          </label>
-          <label>
-            Appliance make
-            <span>What make is your appliance?</span>
-            <select
-              id="applianceMake"
-              name="applianceMake"
-              onChange={formik.handleChange}
-              value={formik.values.applianceMake}
-            >
-              {applianceMakes.map(make => (
-                <option key={make.label} value={make.value}>
-                  {make.label}
-                </option>
-              ))}
-            </select>
-          </label>
-        </div>        
-        <label>
-          Problem with your appliance
-          <span>Tell us what's wrong with your appliance</span>
-          <textarea
-            id="applianceProblem"
-            name="applianceProblem"
-            onChange={formik.handleChange}
-            value={formik.values.applianceProblem}
-            rows="6"
-          ></textarea>
-        </label>
-        <div className="multi-column">
-          <label>
-            Preferred Date for Repair
-            <span>When should our technician visit?</span>
-            <SingleDatePicker
-              numberOfMonths={1}
-              date={date} // momentPropTypes.momentObj or null
-              onDateChange={date => setDate(date)} // PropTypes.func.isRequired
-              focused={focused} // PropTypes.bool
-              onFocusChange={({ focused }) => setFocused(focused)} // PropTypes.func.isRequired
-              id="preferredDateForRepair" // PropTypes.string.isRequired,
-              openDirection={OPEN_UP}
-              noBorder={true}
-              block={true}
-              displayFormat="dddd, DD MMMM YYYY"
-            />
-          </label>
-          <label>
-            Preferred Time for Repair
-            <span>What time is best for you?</span>
-            <TimePicker id="preferredTimeForRepair" />
-          </label>
-        </div>
-        <input type="submit" value="Submit booking" />
-      </form>
+    <iframe
+      id="JotFormIFrame-251002651717347"
+      title="Engage Electronics - Booking Form"
+      onload="window.parent.scrollTo(0,0)"
+      allowtransparency="true"
+      allow="geolocation; microphone; camera; fullscreen"
+      src="https://form.jotform.com/251002651717347"
+      frameborder="0"
+      style="min-width:100%;max-width:100%;height:539px;border:none;"
+      scrolling="no"
+    >
+    </iframe>
+    <script src='https://cdn.jotfor.ms/s/umd/latest/for-form-embed-handler.js'></script>
+    <script>window.jotformEmbedHandler("iframe[id='JotFormIFrame-251002651717347']", "https://form.jotform.com/")</script>
     </React.Fragment>
   );
 };
